b6f2e568b127f5a791a1b81c99b3d42bb36de5a3
Purvesh Rathod <purvesh62@github.com>
2019-12-20 11:42:41 +0530

    Post added

--- flask-intro.py
+++ flask-intro.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Purvesh Rathod',
+        'title': 'Machine Learning',
+        'content': 'Machine Learning Daily',
+        'date_posted': 'December 20, 2019'
+    },
+    {
+        'author': 'Harsh Mishra',
+        'title': 'Java Programming',
+        'content': 'Java Daily',
+        'date_posted': 'December 20, 2019'
+    },
+
+]
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',posts = posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title = 'About')
 
 if __name__ == '__main__':
     app.run()
